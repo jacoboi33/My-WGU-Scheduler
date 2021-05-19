b334bc678b5013e667d2aa6a5a94cd79deb71196
jacob <jisaacs46@gmail.com>
2021-05-19 15:38:10 -0600

    updates

--- app/src/main/java/scheduler/wgu/mywguscheduler/UI/InstructorDetailActivity.java
+++ app/src/main/java/scheduler/wgu/mywguscheduler/UI/InstructorDetailActivity.java
@@ -3,6 +3,13 @@ package scheduler.wgu.mywguscheduler.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.List;
+
 import scheduler.wgu.mywguscheduler.R;
 
 public class InstructorDetailActivity extends AppCompatActivity {
@@ -11,5 +18,35 @@ public class InstructorDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_detail);
+
+        View rootView = findViewById(android.R.id.content);
+
+        final List<TextInputLayout> textInputLayouts = Utils.findViewsWithType(
+                rootView, TextInputLayout.class);
+
+        Button button = findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean noErrors = true;
+                for (TextInputLayout textInputLayout : textInputLayouts) {
+                    String editTextString = textInputLayout.getEditText().getText().toString();
+                    if (editTextString.isEmpty()) {
+                        textInputLayout.setError(getResources().getString(R.string.error_string));
+                        noErrors = false;
+                    } else {
+                        textInputLayout.setError(null);
+                    }
+                }
+
+                if (noErrors) {
+                    // All fields are valid!
+                }
+            }
+        });
+    }
+
+    public void addInstructor(View view) {
+
     }
 }
